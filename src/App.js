b3f3cf17a2bf042524da907383c7611af90559b8
chaosmos <vitovich1987@gmail.com>
2022-09-10 02:04:38 +0300

    upgraded the project to react-router version 6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
 import {
@@ -21,27 +21,46 @@ function App() {
         <Router>
           <Navbar/>
           <Sidebar/>
-          <Switch>
-            <Route exact path="/">
-              <Home/>
-            </Route>
-            <Route exact path="/about">
-              <About/>
-            </Route>
-            <Route exact path="/cart">
-              <Cart/>
-            </Route>
-            <Route exact path="/products">
-              <Products/>
-            </Route>
-            <Route exact path="/products/:id" children={<SingleProduct/>}/>
-            <PrivateRoute exact path="/checkout">
-              <Checkout/>
-            </PrivateRoute>
-            <Route path="*">
-              <Error/>
-            </Route>
-          </Switch>
+          <Routes>
+            {/* react router version 5 */}
+            {/*<Route exact path="/">*/}
+            {/*  <Home/>*/}
+            {/*</Route>*/}
+            {/*<Route exact path="/about">*/}
+            {/*  <About/>*/}
+            {/*</Route>*/}
+            {/*<Route exact path="/cart">*/}
+            {/*  <Cart/>*/}
+            {/*</Route>*/}
+            {/*<Route exact path="/products">*/}
+            {/*  <Products/>*/}
+            {/*</Route>*/}
+            {/*<Route exact path="/products/:id" children={<SingleProduct/>}/>*/}
+            {/*<PrivateRoute exact path="/checkout">*/}
+            {/*  <Checkout/>*/}
+            {/*</PrivateRoute>*/}
+            {/*<Route path="*">*/}
+            {/*  <Error/>*/}
+            {/*</Route>*/}
+            
+            {/*  react router version 6 */}
+            
+            <Route path="/" element={<Home/>}/>
+            <Route path="about" element={<About/>}/>
+            <Route path="cart" element={<Cart/>}/>
+            <Route path="products" element={<Products/>}/>
+            <Route path="products/:id" element={<SingleProduct/>}/>
+            <Route
+                path="checkout"
+                element={
+                  <PrivateRoute>
+                    <Checkout/>
+                  </PrivateRoute>
+                }
+            />
+            <Route path="*" element={<Error/>}/>
+          
+          </Routes>
           <Footer/>
         </Router>
       </AuthWrapper>
